331d515fad0b9d24768f9b42c5e92e0b8a62bb5e
Vitali Avagyan <eheva87@gmail.com>
2023-12-03 04:45:56 +0400

    feat: add speculative decoding

--- src/petals/client/remote_generation.py
+++ src/petals/client/remote_generation.py
@@ -36,7 +36,102 @@ class _SkipTokensMixin:
         _skipped_tokens.set(0)
         return super().prepare_inputs_for_generation(input_ids, **kwargs)
 
+def multinomial_sample_one_no_sync(probs_sort): # Does multinomial sampling without a cuda synchronization
+    q = torch.empty_like(probs_sort).exponential_(1)
+    return torch.argmax(probs_sort / q, dim=-1, keepdim=True).to(dtype=torch.int)
 
+def logits_to_probs(logits, temperature: float = 1.0, top_k: Optional[int] = None):
+    logits = logits / max(temperature, 1e-5)
+
+    if top_k is not None:
+        v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
+        pivot = v.select(-1, -1).unsqueeze(-1)
+        logits = torch.where(logits < pivot, -float("Inf"), logits)
+    probs = torch.nn.functional.softmax(logits, dim=-1)
+    return probs
+
+def sample(logits, temperature: float = 1.0, top_k: Optional[int] = None):
+    probs = logits_to_probs(logits[0, -1], temperature, top_k)
+    idx_next = multinomial_sample_one_no_sync(probs)
+    return idx_next, probs
+
+def prefill(model: Transformer, x: torch.Tensor, input_pos: torch.Tensor, **sampling_kwargs) -> torch.Tensor:
+    # input_pos: [B, S]
+    logits = model(x, input_pos)
+    return sample(logits, **sampling_kwargs)[0]
+
+def decode_one_token(model: Transformer, x: torch.Tensor, input_pos: torch.Tensor, **sampling_kwargs) -> Tuple[torch.Tensor, torch.Tensor]:
+    # input_pos: [B, 1]
+    assert input_pos.shape[-1] == 1
+    logits = model(x, input_pos)
+    return sample(logits, **sampling_kwargs)
+    
+def decode_n_tokens(model: Transformer, cur_token: torch.Tensor, input_pos: torch.Tensor, num_new_tokens: int, callback=lambda _: _, **sampling_kwargs):
+    new_tokens, new_probs = [], []
+    for i in range(num_new_tokens):
+        with torch.backends.cuda.sdp_kernel(enable_flash=False, enable_mem_efficient=False, enable_math=True): # Actually better for Inductor to codegen attention here
+            next_token, next_prob = decode_one_token(
+                model, cur_token, input_pos, **sampling_kwargs
+            )
+        input_pos += 1
+        new_tokens.append(next_token.clone())
+        callback(new_tokens[-1])
+        new_probs.append(next_prob.clone())
+        cur_token = next_token.view(1, -1)
+    return new_tokens, new_probs
+
+def model_forward(model, x, input_pos):
+    return model(x, input_pos)
+def speculative_decode(
+    model: Transformer,
+    draft_model: Transformer,
+    cur_token: torch.Tensor,
+    input_pos: int,
+    speculate_k: int,
+    **sampling_kwargs
+) -> torch.Tensor:
+    # draft model inference sequentially
+    device = cur_token.device
+    orig_input_pos = torch.tensor([input_pos], dtype=torch.int64, device=cur_token.device)
+    draft_tokens, draft_probs = decode_n_tokens(draft_model, cur_token.view(1, -1), orig_input_pos.clone(), speculate_k, **sampling_kwargs)
+
+    draft_tokens = torch.cat(draft_tokens)
+    # parallel inference on target model using draft tokens
+    target_logits = model_forward(
+        model,
+        torch.cat([cur_token.view(1), draft_tokens]).view(1, -1),
+        torch.arange(input_pos, input_pos + speculate_k + 1, device=cur_token.device)
+    )
+    target_probs = logits_to_probs(target_logits[0], **sampling_kwargs)
+    draft_probs = torch.stack(draft_probs)
+    # q: target prob, p: draft prob
+    # q >= p: always accept draft token
+    # q < p: q/p prob to accept draft token
+    p = draft_probs[torch.arange(0, speculate_k, device=device), draft_tokens]
+    q = target_probs[torch.arange(0, speculate_k, device=device), draft_tokens]
+    accept_draft_prob = torch.minimum(torch.ones(()), q[:speculate_k]/ p)
+    rejected_locations = (torch.rand_like(accept_draft_prob) > accept_draft_prob).nonzero()
+
+    if rejected_locations.shape[0] == 0: # All draft tokens have been accepted
+        accept_length = speculate_k + 1
+        last_token = multinomial_sample_one_no_sync(target_probs[-1])
+        # fill last token into draft model
+        model_forward(
+            draft_model,
+            draft_tokens[-1].view(1, -1),
+            orig_input_pos + speculate_k,
+        )
+        return torch.cat([draft_tokens, last_token])
+    else:
+        accept_length = rejected_locations[0].item()
+        p = draft_probs[accept_length]
+        q = target_probs[accept_length]
+        new = q - p
+        new = torch.where(new > 0, new, 0.0)
+        new = new / new.sum()
+        next_token = multinomial_sample_one_no_sync(new)
+        return torch.cat([draft_tokens[:accept_length], next_token])
+            
 class RemoteGenerationMixin(_SkipTokensMixin):
     """
     This class is an upgrade to `transformers.GenerationMixin` that:
@@ -66,7 +161,9 @@ class RemoteGenerationMixin(_SkipTokensMixin):
 
     @docstring_from(transformers.GenerationMixin.generate.__doc__)
     def generate(
-        self, inputs: Optional[torch.Tensor] = None, *args, session: Optional[InferenceSession] = None, **kwargs
+        self, inputs: Optional[torch.Tensor] = None, *args, session: Optional[InferenceSession] = None, 
+        draft_model = None, 
+        speculate_k: Optional[int] = None,**kwargs
     ):
         self._fix_generate_kwargs(kwargs)
         if inputs is None:
@@ -112,20 +209,45 @@ class RemoteGenerationMixin(_SkipTokensMixin):
                 # Don't actually run all previous tokens through the transformer,
                 # but keep them for transformers.GenerationMixin (e.g., to compute repetition_penalty)
                 _skipped_tokens.set(max(0, n_prev_tokens - 1))
+            
+            # Now integrate the speculative decoding logic
+            is_speculative = draft_model is not None and speculate_k is not None
+            generated_sequence = []
 
-            result = super().generate(inputs, *args, **kwargs)
+            if is_speculative:
+                cur_token = inputs
+                input_pos = 0
+                desired_length = kwargs.get('max_length', 50)
 
-            sequences = result.sequences if isinstance(result, ModelOutput) else result
-            # Save tokens from this .generate() call
-            session.output_ids = sequences
-            # Crop the last tokens from the previous call
-            sequences = sequences[:, n_prev_tokens:].clone()
-            if isinstance(result, ModelOutput):
-                result.sequences = sequences
+                while input_pos < desired_length:
+                    next_tokens = speculative_decode(
+                        self, draft_model, cur_token, input_pos, speculate_k, **kwargs
+                    )
+
+                    generated_sequence.extend(next_tokens.tolist())
+                    cur_token = next_tokens[-1].unsqueeze(0)
+                    input_pos += len(next_tokens)
+
+                    if cur_token.item() == self.config.eos_token_id:
+                        break
+
+                generated_sequence_tensor = torch.tensor(generated_sequence, dtype=inputs.dtype, device=inputs.device)
+                result = generated_sequence_tensor.unsqueeze(0)
+                return result
             else:
-                result = sequences
+                result = super().generate(inputs, *args, **kwargs)
 
-        return result
+                sequences = result.sequences if isinstance(result, ModelOutput) else result
+                # Save tokens from this .generate() call
+                session.output_ids = sequences
+                # Crop the last tokens from the previous call
+                sequences = sequences[:, n_prev_tokens:].clone()
+                if isinstance(result, ModelOutput):
+                    result.sequences = sequences
+                else:
+                    result = sequences
+
+            return result
 
     @staticmethod
     def _fix_generate_kwargs(kwargs: dict):
